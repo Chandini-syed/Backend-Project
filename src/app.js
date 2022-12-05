aac82dab07b7edf9e3cdac2a5c46c433d3cf67ec
Chandini-syed <chandusyed@gmail.com>
2022-12-05 23:17:33 +0530

    Code updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-
+const Subscribers = require('./models/subscribers')
 const express = require('express');
 const app = express()
 
 
-// Your code goes here
+// Get Home Page message in the home page
+
+app.get('/',(req,res)=>{
+    res.json('Home Page')
+})
 
 
+app.get("/subscribers", (req, res) => { 
+    const subscribers = req.params; 
+    Subscribers 
+      .find(subscribers) 
+      .then((data) => { 
+        res.json(data); 
+      }) 
+      .catch((err) => { 
+        console.log(err); 
+      }); 
+  });
+
+  app.get("/subscribers/names", (req, res) => { 
+    Subscribers 
+    .find({}).select('-_id -subscribedDate -__v')   
+    .then((data) => { 
+      res.json(data); 
+    }) 
+    .catch((err) => { 
+      console.log(err); 
+    }); 
+  });
+
+
+  app.get('/subscribers/:id', async (req, res) => {
+    const _id =req.params.id
+    try{
+        const data = await   Subscribers .findById(_id);
+
+        if(!data) {
+            error = Error ('error statement')
+            res.status(400).json({message :error.message} )
+            }
+             else { res.json(data)}
+
+         } catch(error){
+        res.status(400).json({message: error.message})
+    }});
 
 
 
